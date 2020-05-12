d0c2e49826b182110f87a8b592aa8f34a37f7f07
lancewburklin <1677@holbertonschool.com>
2020-05-12 18:29:17 +0000

    Initial commit of main file

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * main - Opens monty files and executes
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: 0 for success, multiple exit statuses
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *fd = NULL;
+
+	if (argc != 2)
+	{
+		write(2, "USAGE: monty file\n", 18);
+		exit(EXIT_FAILURE);
+	}
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		write(2, "Error: Can't open file ", 23);
+		write(2, argv[1], strlen(argv[1]));
+		write(2, "\n", 1);
+		exit(EXIT_FAILURE); 
+	}
+	close(fd);
+	return (0);
+}
